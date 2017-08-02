e3e3acf0a0f5ef7311003e2c1fcab6c48f86dd5a
cris <cris@dosy>
2017-08-02 06:16:26 +0000

    Added a webview controller class
    Correctly set the user agent for the webview

---
+++ webview_controller.js
@@ -0,0 +1,29 @@
+"use strict";
+{
+  const CURRENT_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36';
+  const WVC = {
+    setup
+  };
+
+  module.exports = WVC;
+
+  function setup( start_url = 'about:blank' ) {
+    const wv = document.querySelector('webview');
+
+    wv.addEventListener('dom-ready', () => {
+      const wc = wv.getWebContents();
+      wc.setUserAgent( CURRENT_UA );
+      wc.loadURL( start_url );
+      Object.assign( self, { wv, wc } );
+    }, { once: true } )
+
+    wv.src="data:text/html,<div>Connecting to the internet...";
+
+    if ( navigator.onLine ) {
+      wv.src+=`<div>Connected</div><div>Navigating to ${start_url}...</div>`;
+    } else {
+      wv.src+=`<div>Unable to connect to the internet.</div>`;
+    }
+  }
+
+}

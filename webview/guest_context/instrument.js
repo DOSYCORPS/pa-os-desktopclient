7843e9e69038f8ae7bca10963f0b6eed11379a30
cris <cris@dosy>
2017-08-03 12:46:33 +0000

    dosycanvasdomtracker clearly shown working
    in test integration without splitting across the guest and embedder context
    
    next step is to split dcdt across two contexts

--- webview/guest_context/instrument.js
+++ webview/guest_context/instrument.js
@@ -5,6 +5,16 @@
     dct    
   };
 
+  test_integration_same_context();
+
+  function test_integration_same_context() {
+    const c = document.createElement('canvas');
+    self.addEventListener('load', () => {
+      document.body.appendChild( c );
+      dct.start_tracking();
+      dct.track_all('a');
+    });
+  }
 
   module.exports = instrument;
 }

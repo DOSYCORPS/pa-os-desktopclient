83d75ca6b2375c548fb1814498197a89f64a7495
cris <cris@dosy>
2017-08-04 03:02:43 +0000

    Added host side of guest-host communication

---
+++ webview/communicator.js
@@ -0,0 +1,21 @@
+"use strict";
+{
+  /* guest-host communication, across webview boundary: host side */
+
+  // We hook any incoming ipc message to window message event
+  // So in the host ipc messages from webview can be subscribed to by using
+  // message event of window
+
+  module.exports = { setup_webview };
+
+  function setup_webview() {
+    self.addEventListener('load', install_ipc_listener );
+  }
+
+  function install_ipc_listener() {
+    const wv = document.querySelector('webview');
+    wv.addEventListener('ipc-message', message => {
+      self.postMessage(message,"*");
+    });
+  }
+}

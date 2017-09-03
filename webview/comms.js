877d4d81025eda8ae5d53e89600a571612a86c94
cris <cris@dosy>
2017-09-03 14:51:30 +0000

    Somewhere along the line

--- webview/comms.js
+++ webview/comms.js
@@ -1,18 +1,44 @@
 "use strict";
 {
-  const ipc = require('electron').ipcRenderer;
+  const isRenderer = process && process.type == 'renderer';
+  const listeners = [];
+  let selfName;
+  let ipc;
+
+  if ( isRenderer ) {
+    ipc = require('electron').ipcRenderer;
+  } else {
+    ipc = require('electron').ipcMain;
+  }
 
   const comms = {
-    send, nameSelf
+    send, listen
   };
+
+  if ( isRenderer ) {
+    Object.assign( comms, { nameSelf } );
+  } else {
+    nameSelf('main');
+  }
 
   module.exports = comms;
 
   function send( name, msg ) {
+    msg.from = selfName;
+    ipc.send(name, msg); 
+  }
 
+  function listen( listener ) {
+    listeners.push( listener );
   }
 
   function nameSelf( name ) {
+    //FIXME : ensure no name conflict
+    selfName = name;
+    ipc.on(selfName, runListeners);
+  }
 
+  function runListeners( msg ) {
+    listeners.forEach( l => l( msg ) );
   }
 }

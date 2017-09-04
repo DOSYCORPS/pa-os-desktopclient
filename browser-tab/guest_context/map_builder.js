b534e3e0105da50dd64380fed771a209c8936ef2
cris <cris@dosy>
2017-09-04 14:58:16 +0000

    Basic integration of positive and negative working

--- browser-tab/guest_context/map_builder.js
+++ browser-tab/guest_context/map_builder.js
@@ -8,10 +8,15 @@
   function install() {
     document.addEventListener('click', e => {
       if ( e.shift || e.shiftKey ) {
+        let type = 'positive';
+        if ( e.alt || e.altKey ) {
+          type = 'negative';
+        }
         e.preventDefault();
         console.log("Shift pressed so preventing default and tracking."); 
         comms.send('build',{
           trackThis : true,
+          type,
           canonicalSel : sg.get_canonical_sel( e.target )
         });
       }
--- webview-component/build.js
+++ webview-component/build.js
@@ -8,5 +8,16 @@
     const api = comms.install();
     Object.assign( self, { comms: api } );
     self.comms.nameSelf('build');
+    self.comms.listen( handle );
+  }
+
+  function handle( event, msg ) {
+    if ( msg.trackThis ) {
+      const {canonicalSel} = msg;
+      const newSel = document.querySelector(`input.new[name="${msg.type}"]`);
+      const save = document.querySelector(`input.new[name="${msg.type}"] ~ button[value="save"]`);
+      newSel.value = canonicalSel;
+      save.click();
+    }
   }
 }

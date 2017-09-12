af276840bd238a483baa07e6a19f03ccc833a6b7
cris <cris@dosy>
2017-09-12 08:58:32 +0000

    Things look good.
    Thanks a lot

--- browser-tab/guest_context/map_builder.js
+++ browser-tab/guest_context/map_builder.js
@@ -11,6 +11,10 @@
       comms.send('workspace', msg);
     };
     dct.guest(true, { sendMessage });
+    dct.add_name('singles', { color: 'rgba(0,200,50,0.6)', style: 'outline'} );
+    dct.add_name('antis', { color: 'rgba(200,0,0,0.6)', style: 'outline'} );
+    dct.add_name('antisets', { color: 'rgba(250,0,0,0.4)', style: 'fill'} );
+    dct.add_name('sets', { color: 'rgba(100,0,200,0.4)', style: 'fill'} );
     
     document.addEventListener('click', e => {
       if ( e.shift || e.shiftKey ) {
@@ -30,34 +34,34 @@
           canonicalSel : sg.get_canonical_sel( e.target )
         };
         comms.send('build', message);
-        let color = 'rgba(0,200,50,0.5)';
         if ( message.type == 'negative' ) {
-          color = 'rgba(200,0,100,0.5)';
+          dct.track(message.canonicalSel, 'antis');
+        } else {
+          dct.track(message.canonicalSel, 'singles');
         }
-        dct.track(message.canonicalSel, {color, style: 'outline'});
       }
     });
     comms.listen( (e,m) => {
       let color;
       if ( m.trackThis ) {
         if ( m.trackAll ) {
-          color = 'rgba(0,0,200,0.5)';
           if ( m.type == 'negative' ) {
-            color = 'rgba(200,0,100,0.5)';
-          }
-          if ( m.canonicalSel ) {
-            dct.track_all(m.canonicalSel, {color});
+            dct.track_all(m.canonicalSel, 'antisets');
+          } else {
+            dct.track_all(m.canonicalSel, 'sets');
           }
         } else {
-          color = 'rgba(0,200,0,0.5)';
           if ( m.type == 'negative' ) {
-            color = 'rgba(200,0,100,0.5)';
+            dct.track(m.canonicalSel, 'antis');
+          } else {
+            dct.track(m.canonicalSel, 'singles');
           }
-          dct.track(m.canonicalSel, {color});
         }
       } else if ( m.untrackThis ) {
-        dct.untrack(m.canonicalSel); 
-        dct.untrack_all(m.generalizedSel);
+        dct.untrack(m.canonicalSel,'singles'); 
+        dct.untrack(m.canonicalSel,'antis'); 
+        dct.untrack_all(m.generalizedSel,'sets');
+        dct.untrack_all(m.generalizedSel,'antisets');
       }
     });
   }

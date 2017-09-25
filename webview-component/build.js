3a20c44ca6f81aff8cad0d35ee6269393c585b6f
Cris <writetocris@outlook.com>
2017-09-25 05:57:11 +0000

    Updated to new iframe iinterface

--- webview-component/build.js
+++ webview-component/build.js
@@ -14,6 +14,50 @@
     Object.assign( self, { comms: api } );
     self.comms.nameSelf('build');
     self.comms.listen( handle );
+    run_task_queue();
+    document.querySelector('#propview').addEventListener('load', run_task_queue);
+    install_delete_handler();
+    loaded = true;
+  }
+
+  function install_delete_handler() {
+    document.addEventListener('click', (e) => {
+      if ( e.target && e.target.matches('button[name$="_delete"]') ) {
+        do_delete(e); 
+      }
+    });
+  }
+
+  function generalize() {
+    console.log("Generalize called");
+    const generalize_btn = frames.propview.document.querySelector('#generalize');
+    setTimeout( () => {
+      generalize_btn.click();
+      setTimeout( () => {
+        const generalized = frames.propview.document.querySelector('#generalized');
+        self.comms.send('browser-tab-guest', {
+          trackThis: true,
+          trackAll: true,
+          canonicalSel: generalized.value
+        });
+      }, 50 );
+    }, 50 );
+  }
+
+  function do_delete(e) {
+    const sel = e.target.parentNode.querySelector('input[type="text"]').value;
+    const gsel = frames.propview.document.querySelector('#generalized').value;
+    self.comms.send('browser-tab-guest', {
+      untrackThis: true,
+      canonicalSel: sel,
+      generalizedSel: gsel
+    });
+    const queue = JSON.parse(localStorage.getItem('q'));
+    queue.push({task: 'generalize'});
+    localStorage.setItem('q',JSON.stringify(queue));
+  }
+
+  function run_task_queue() {
     let queue;
     try {
       queue = JSON.parse(localStorage.getItem('q'));
@@ -32,45 +76,6 @@
       }
     }
     localStorage.setItem('q', '[]');
-
-    install_delete_handler();
-
-    function install_delete_handler() {
-      document.addEventListener('click', (e) => {
-        if ( e.target && e.target.matches('button[name$="_delete"]') ) {
-          do_delete(e); 
-        }
-      });
-    }
-
-    function generalize() {
-      const generalize_btn = frames.propview.document.querySelector('#generalize');
-      setTimeout( () => {
-        generalize_btn.click();
-        setTimeout( () => {
-          const generalized = frames.propview.document.querySelector('#generalized');
-          self.comms.send('browser-tab-guest', {
-            trackThis: true,
-            trackAll: true,
-            canonicalSel: generalized.value
-          });
-        }, 50 );
-      }, 50 );
-    }
-
-    function do_delete(e) {
-      const sel = e.target.parentNode.querySelector('input[type="text"]').value;
-      const gsel = frames.propview.document.querySelector('#generalized').value;
-      self.comms.send('browser-tab-guest', {
-        untrackThis: true,
-        canonicalSel: sel,
-        generalizedSel: gsel
-      });
-      const queue = JSON.parse(localStorage.getItem('q'));
-      queue.push({task: 'generalize'});
-      localStorage.setItem('q',JSON.stringify(queue));
-    }
-    loaded = true;
   }
 
   function handle( event, msg ) {
